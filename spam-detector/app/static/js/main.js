60a0a7b36cf5950f6c33346d9e9ac97da17203d4
Francisco Joshua Quintero Montero <143279487+joshuaqm@users.noreply.github.com>
2026-05-25 14:42:29 -0600

    Remove spam-detector directory
    Delete the entire spam-detector directory and its contents because it was moved into ProyectoFinal_Datanova.

--- spam-detector/app/static/js/main.js
+++
@@ -1,8 +0,0 @@
-function openSidebar() {
-  document.getElementById('sidebar').classList.add('open');
-  document.getElementById('sidebar-overlay').classList.add('open');
-}
-function closeSidebar() {
-  document.getElementById('sidebar').classList.remove('open');
-  document.getElementById('sidebar-overlay').classList.remove('open');
-}
